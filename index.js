bc0395f23e461205ca041f6046c73e4475c618c5
hardik devmurari <48158878+hardikdevmurari@users.noreply.github.com>
2019-05-20 16:29:46 +0530

    chan

---
+++ index.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+	host:"localhost",
+	user:"root",
+	password:"",
+	database: "nodedb"
+});
+
+// con.connect(function (err){
+// 	if (err) throw err;
+// 	console.log("connnceted");
+// });
+
+//--------data base creat-----
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   con.query("CREATE DATABASE nodedb", function (err, result) {
+//     if (err) throw err;
+//     console.log("Database created");
+//   });
+// });
+
+con.connect(function(err){
+	if (err) throw err;
+	console.log("connceted");
+	const sql = "CREATE TABLE students (name VARCHAR(255), roll VARCHAR(255))";
+	con.query(sql,function(err,result){
+		if (err) throw err;
+		console.log("table created");
+	});
+});
